83bb01754c6b7316f4dd3c2e914cc5d8233d66b5
Michael Grosser <michael@grosser.it>
2018-06-16 14:49:07 -0700

    test

--- pkg.go
+++ pkg.go
@@ -1,11 +1,68 @@
 package go_scov
 
-var result string
+import (
+	"io/ioutil"
+	"strings"
+	"os/exec"
+	"fmt"
+)
 
-func Setup() {
-
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
 
-func GetResult() string {
-	return result
+// "" => []  "foo" => ["foo"]
+func splitWithoutEmpty(string string, delimiter rune) []string {
+	return strings.FieldsFunc(string, func(c rune) bool { return c == delimiter })
+}
+
+func filter(ss []string, test func(string) bool) []string {
+	ret := []string{}
+	for _, s := range ss {
+		if test(s) {
+			ret = append(ret, s)
+		}
+	}
+	return ret
+}
+
+func CovTest(argv []string) bool {
+	file := "coverage.out"
+	argv = append([]string{"go", "test"}, argv...)
+	argv = append(argv, "-cover", "-coverprofile=" + file)
+	success := RunCommand(argv)
+	// TODO: parse the coverage
+	// TODO: print the coverage
+	// TODO: set success to fail when there is missing coverage
+	return success
+}
+
+// run argv and return if it succeeded
+// TODO: show out and err to the user as they appear
+func RunCommand(argv []string) bool {
+	command := argv[0]
+	out, err := exec.Command(command, argv[1:]...).Output()
+	fmt.Print(string(out))
+	return err == nil
+}
+
+// Find the uncovered lines given a coverage file path
+func Uncovered(path string) []string {
+	data, err := ioutil.ReadFile(path)
+	check(err)
+
+	lines := splitWithoutEmpty(string(data), '\n')
+	if(len(lines) == 0) {
+		return []string{}
+	}
+
+	// remove the initial `set: mode` line
+	lines = lines[1:]
+
+	// filter out lines that are covered (end " 0")
+	lines = filter(lines, func(line string) bool { return strings.HasSuffix(line, " 0") })
+
+	return lines
 }
--- pkg_test.go
+++ pkg_test.go
@@ -3,7 +3,11 @@ package go_scov
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"os"
 	"testing"
+	"bytes"
+	"io"
 )
 
 func TestAwesome(t *testing.T) {
@@ -11,12 +15,127 @@ func TestAwesome(t *testing.T) {
 	RunSpecs(t, "Example")
 }
 
-var _ = Describe("Example", func() {
-	Describe("Make an awesome expression", func() {
-		Context("smile", func() {
-			It("should result :)", func() {
-				GetResult()
-				Expect(":)").To(Equal(":)"))
+func noError(err error) {
+	Expect(err).To(BeNil())
+}
+
+func writeFile(path string, content string) {
+	err := ioutil.WriteFile(path, []byte(content), 0755)
+	noError(err)
+}
+
+func withTempfile(content string, fn func(*os.File)) {
+	file, err := ioutil.TempFile("", "go-scov")
+	noError(err)
+	defer os.Remove(file.Name())
+	writeFile(file.Name(), content)
+	fn(file)
+}
+
+func withTempDir(fn func(string)) {
+	dir, err := ioutil.TempDir("", "go-scov")
+	noError(err)
+	defer os.RemoveAll(dir)
+	fn(dir)
+}
+
+func withEnv(key string, value string, fn func()) {
+	old := os.Getenv(key)
+	os.Setenv(key, value)
+	defer os.Setenv(key, old)
+	fn()
+}
+
+func withFakeGo(content string, fn func()) {
+	withTempDir(func(dir string){
+		withEnv("PATH", dir, func(){
+			writeFile(dir + "/go", "#!/bin/sh\n" + content)
+			fn()
+		})
+	})
+}
+
+// https://stackoverflow.com/questions/10473800/in-go-how-do-i-capture-stdout-of-a-function-into-a-string
+func captureStdout(fn func()) string {
+	old := os.Stdout // keep backup of the real stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	fn()
+
+	outC := make(chan string)
+	// copy the output in a separate goroutine so printing can't block indefinitely
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	// back to normal state
+	w.Close()
+	os.Stdout = old // restoring the real stdout
+	return <-outC
+}
+
+var _ = Describe("go_scov", func() {
+	Describe("CovTest", func(){
+		It("adds coverage to passed in arguments", func(){
+			withFakeGo("touch coverage.out\necho go \"$@\"", func(){
+				success := false
+				output := captureStdout(func(){
+					success = CovTest([]string{"hello", "world"})
+				})
+				Expect(success).To(Equal(true))
+				Expect(output).To(Equal("go test hello world -cover -coverprofile=coverage.out\n"))
+			})
+		})
+		// TODO: fails without adding noise
+		// TODO: does not fail when coverage is ok
+		// TODO: fail when coverage is not ok
+	})
+
+	Describe("RunCommand", func(){
+		It("Runs the given command", func() {
+			success := false
+			output := captureStdout(func(){
+				success = RunCommand([]string{"echo", "123"})
+			})
+			Expect(success).To(Equal(true))
+			Expect(output).To(Equal("123\n"))
+		})
+
+		It("returns false when command fails", func() {
+			success := true
+			output := captureStdout(func(){
+				success = RunCommand([]string{"ls", "--nope"})
+			})
+			Expect(success).To(Equal(false))
+			Expect(output).To(Equal(""))
+		})
+	})
+
+	Describe("Uncovered", func() {
+		It("shows nothing for empty", func() {
+			withTempfile("", func(file *os.File){
+				Expect(Uncovered(file.Name())).To(Equal([]string{}))
+			})
+		})
+
+		It("shows uncovered", func(){
+			withTempfile("mode: set\nfoo/pkg.go:1.2,3.4 1 0\n", func(file *os.File){
+				Expect(Uncovered(file.Name())).To(Equal([]string{"foo/pkg.go:1.2,3.4 1 0"}))
+			})
+		})
+
+		It("does not show covered", func(){
+			withTempfile("mode: set\nfoo/pkg.go:1.2,3.4 1 1\n", func(file *os.File){
+				Expect(Uncovered(file.Name())).To(Equal([]string{}))
+			})
+		})
+
+		It("does not show covered even if coverage ends in 0", func(){
+			withTempfile("mode: set\nfoo/pkg.go:1.2,3.4 1 10\n", func(file *os.File){
+				Expect(Uncovered(file.Name())).To(Equal([]string{}))
 			})
 		})
 	})
